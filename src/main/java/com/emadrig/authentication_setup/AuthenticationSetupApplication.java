00ba32d74c00a77932a76b37dcbea7651c95935e
Edwin Madrigal <emadrig1@uci.edu>
2024-07-23 12:26:33 -0700

    Initial commit

---
+++ src/main/java/com/emadrig/authentication_setup/AuthenticationSetupApplication.java
@@ -0,0 +1,13 @@
+package com.emadrig.authentication_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSetupApplication.class, args);
+	}
+
+}
