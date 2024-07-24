3e320d53e51c0cb9a36490d2eb0406b926501038
Edwin Madrigal <emadrig1@uci.edu>
2024-07-24 00:34:04 -0700

    updated User class as well as user repository, created Jwt authentication filter

---
+++ src/main/java/com/emadrig/authentication_setup/entities/Role.java
@@ -0,0 +1,7 @@
+package com.emadrig.authentication_setup.entities;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
--- src/main/java/com/emadrig/authentication_setup/repositories/UserRepository.java
+++ src/main/java/com/emadrig/authentication_setup/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package com.emadrig.authentication_setup.repositories;
 
 import com.emadrig.authentication_setup.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository  extends CrudRepository<User, Integer> {
+public interface UserRepository  extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
