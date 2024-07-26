d7d7f06e377bc45f76ccd13d3f43fcaf46532e7d
Edwin Madrigal <emadrig1@uci.edu>
2024-07-25 21:03:52 -0700

    completed authenticate and authorize endpoints, created demo controller to test

--- src/main/java/com/emadrig/authentication_setup/config/JwtAuthenticationFilter.java
+++ src/main/java/com/emadrig/authentication_setup/config/JwtAuthenticationFilter.java
@@ -25,11 +25,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
-
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
